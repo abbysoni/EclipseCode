d44a6e2bdb6a74cf52296c9241f05e4ec0ebba0b
unknown <a9094855@gmail.com>
2024-10-16 12:47:27 +0100

    Driver Added

--- helloWorld/FirstClass.java
+++ helloWorld/FirstClass.java
@@ -1,9 +1,24 @@
 package helloWorld;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world A!");
+		
+		WebDriver driver = (WebDriver) new ChromeDriver();
+        
+        System.out.println("none of you know what you're doing");
+
+        driver.get("https://www.google.com");
+        driver.quit();
+
+        System.out.println("Finaaly its over");
 	}
 
 }
+
+
